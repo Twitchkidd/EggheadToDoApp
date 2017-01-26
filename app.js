6872189c22181a864a1b017c2b478dcedd77e249
Twitchkidd <field.gareth@gmail.com>
2017-01-26 15:11:10 -0500

    Lesson 13: Edit items inline

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ class App extends Component {
     this.handleToggleComplete = this.handleToggleComplete.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
     this.handleClearComplete = this.handleClearComplete.bind(this);
+    this.handleUpdateText = this.handleUpdateText.bind(this);
+    this.handleToggleEditing = this.handleToggleEditing.bind(this);
   }
   componentWillMount() {
     AsyncStorage.getItem("items").then((json) => {
@@ -40,6 +42,26 @@ class App extends Component {
         this.setState({ loading: false });
       }
     })
+  }
+  handleUpdateText(key, text) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        text
+      }
+    });
+    this.setSource(newItems, filterItems(this.state.filter, newItems));    
+  }
+  handleToggleEditing(key, editing) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item,
+        editing
+      }
+    });
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
@@ -111,6 +133,8 @@ class App extends Component {
               return (
                 <Row
                   key={key}
+                  onUpdate={(text) => this.handleUpdateText(key, text)}
+                  onToggleEdit={(editing) => this.handleToggleEditing(key, editing)}
                   onRemove={() => this.handleRemoveItem(key)}
                   onComplete={(complete) => this.handleToggleComplete(key, complete)}
                   {...value}
